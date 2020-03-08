7bffe847ebb4a8546a5bc3bf9ead5d578e9afe40
ksahlq <khalid.sahlqeath@outlook.com>
2020-03-09 00:14:18 +0100

    Added test to workflow of ordering items

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,46 +1,65 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add("login", (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add("drag", { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add("dismiss", { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
-
-Cypress.Commands.add("getCreateAccountCookies", () => {
-    cy.getCreateAccountToken()
-    cy.getCookies()
-    .should('have.length', 1)
-    .then((cookies) => {
-        expect(cookies[0]).to.have.property('name', 'PrestaShop-a30a9934ef476d11b6cc3c983616e364')
-    })
-
+Cypress.Commands.add('login', () => {   
+    cy.request({
+        method: 'POST',
+        url: '/index.php?controller=authentication',
+        form: true,
+        body: {
+            email: 'sadtimes@sadtimes.com',
+            passwd: '12345',
+            back: 'my-account',
+            SubmitLogin: '' 
+        }
+    })     
 })
 
-Cypress.Commands.add("getCreateAccountToken", () => {
-    return cy
-    .request({
+Cypress.Commands.add('createAccount', () => {
+    cy.request({
         method: 'POST',
-        url: 'http://automationpractice.com/index.php',
-        followRedirect: true,
-        form: true, 
-        body: "controller=authentication&SubmitCreate=1&ajax=true&email_create=hi.no%40input.input&back=my-account&token=ce65cefcbafad255f0866d3b32d32058"        
+        url: '/index.php?controller=authentication',
+        form: true,
+        body: {
+            customer_firstname: 'Fun',
+            customer_lastname: 'Times',
+            email: 'funtimes@funtimes.com',
+            passwd: '12345',
+            days: '', 
+            months: '',
+            years: '',
+            firstname: 'Fun',
+            lastname: 'Times',
+            company: '',
+            address1: 'rty 55',
+            address2: '',
+            city: 'LA',
+            id_state: '5',
+            postcode: '89012',
+            id_country: '21',
+            other: '',
+            phone: '',
+            phone_mobile: '55552222',
+            alias: 'Waterbloom 65',
+            dni: '',
+            email_create: '1',
+            is_new_customer: '1',
+            back: 'my-account', 
+            submitAccount: ''
+        }
     })
+    cy.getCookie('PrestaShop-a30a9934ef476d11b6cc3c983616e364').should('exist') 
+})
+
+Cypress.Commands.add('addItemToCart', () => {
+    cy.request({
+        method: 'POST',
+        url: '/index.php?rand=1583699244399',
+        form: true, 
+        body: {
+            controller: 'cart',
+            add: '1',
+            ajax: 'true',
+            qty: '1',
+            id_product: '5',
+            token: '70388fa2a1997156b3393a4b6484f52d'
+        }
+    })    
 })
